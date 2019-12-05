c9bb90a91fc82310f7b04310c57019a13e4f0fb2
mvs5465 <mvs5465@gmail.com>
2019-12-05 14:57:31 -0500

    Update nginx template and supporting files with proxy_pass resolver support

--- build-and-run-docker.sh
+++ build-and-run-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t codehub-admin-ui:latest .
-docker run --rm -d -p 8097:80 -e PROXY_PASS_URL=http://localhost:3007/api codehub-admin-ui:latest
+docker run --rm -d -p 8097:80 -e PROXY_PASS='proxy_pass "http://localhost:3007/api";' codehub-admin-ui:latest
